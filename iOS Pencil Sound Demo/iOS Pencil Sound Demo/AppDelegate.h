ef7da3c05d53d0dd80d44330786fdaf5758af898
Nicolás Miari <nicolasmiari@gmail.com>
2014-04-14 11:32:09 +0900

    - Created Xcode workspace. - Created empty Xcode project.

---
+++ iOS Pencil Sound Demo/iOS Pencil Sound Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS Pencil Sound Demo
+//
+//  Created by n.miari on 4/14/14.
+//  Copyright (c) 2014 Nicolas Miari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
