500790b68385e2b89dd23e0909339ecff57260d7
Nicolás Miari <nicolasmiari@gmail.com>
2014-04-14 14:02:35 +0900

    - Got first working version.

--- Source/iOS Pencil Sound Demo/Project Files/Classes/TestScene.m
+++ Source/iOS Pencil Sound Demo/Project Files/Classes/TestScene.m
@@ -8,46 +8,206 @@
 
 #import "TestScene.h"
 
-@implementation TestScene
+#import "SoundManager.h"
 
--(id)initWithSize:(CGSize)size {    
-    if (self = [super initWithSize:size]) {
+
+// .............................................................................
+
+@implementation TestScene
+{
+
+    CFTimeInterval      _currentTime;
+    CFTimeInterval      _dt;            // time since last frame
+    
+    UITouch*            _touch;
+    
+    BOOL                _dragging;
+    CGPoint             _dragLastPosition;
+    CFTimeInterval      _dragLastTime;
+    CGFloat             _dragCurrentSpeed;
+    
+    CFTimeInterval      _lastTime;
+    
+    CFTimeInterval      _blinkCounter;
+    
+    BOOL                _dirty;
+    
+    BOOL                _transitioning;
+    
+    BOOL                _playingSound;
+}
+
+// .............................................................................
+
+-(id) initWithSize:(CGSize) size
+{
+    if ((self = [super initWithSize:size])) {
         /* Setup your scene here */
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        [self setBackgroundColor:[UIColor whiteColor]];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        SKLabelNode* myLabel = [SKLabelNode labelNodeWithFontNamed:@"Avenir"];
         
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
+        [myLabel setText:@"(Drag Anywhere)"];
+        [myLabel setFontSize:30.0f];
+        [myLabel setFontColor:[UIColor darkGrayColor]];
+        [myLabel setPosition:CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))];
         
         [self addChild:myLabel];
     }
+    
     return self;
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
+// .............................................................................
+
+-(void) touchesBegan:(NSSet*) touches
+           withEvent:(UIEvent*) event
+{
+    if (_touch) {
+        // Single touch
+        
+        return;
+    }
     
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
+    
+    _touch   = [touches anyObject];
+
+    CGPoint location = [_touch locationInNode:self];
+    
+    
+    _dragCurrentSpeed = 0.0f;
+    _dragLastPosition = location;
+    _dragLastTime     = _currentTime;
+}
+
+// .............................................................................
+
+- (void) touchesMoved:(NSSet*) touches
+            withEvent:(UIEvent*) event
+{
+    for (UITouch* touch in touches) {
         
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+        if (touch != _touch) {
+            continue;
+        }
         
-        sprite.position = location;
+        _dragging = YES;
         
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+        CGPoint dragNewPosition = [_touch locationInNode:self];
+    
+        CGPoint deltaDrag = CGPointMake(dragNewPosition.x - _dragLastPosition.x,
+                                        dragNewPosition.y - _dragLastPosition.y);
         
-        [sprite runAction:[SKAction repeatActionForever:action]];
+        CGFloat dragLength = sqrtf((deltaDrag.x * deltaDrag.x) + (deltaDrag.y * deltaDrag.y));
         
-        [self addChild:sprite];
+        CFTimeInterval dragTime = _currentTime - _dragLastTime;
+        
+        CGFloat dragSpeed;
+        
+        if (dragTime > 0.001f) {
+            dragSpeed = dragLength / dragTime;
+        }
+        else{
+            dragSpeed = 0.0f;
+        }
+        
+        
+        _dragCurrentSpeed = 0.75*_dragCurrentSpeed + 0.25*dragSpeed;
+
+        
+        // Overwrite for next call:
+        _dragLastPosition = dragNewPosition;
+        _dragLastTime     = _currentTime;
+        
+        
+        SoundManager* soundManager = [SoundManager defaultManager];
+        
+        if (!_playingSound) {
+            // Not playing
+            
+            if (dragSpeed >= 10.0f) {
+                
+                [soundManager playNoise];
+                
+                [soundManager setNoiseGain:0.0f];
+                
+                _playingSound = YES;
+            }
+        }
+        
+        // Skip further touches
+        return;
     }
 }
 
--(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
+// .............................................................................
+
+- (void) touchesEnded:(NSSet*) touches
+            withEvent:(UIEvent*) event
+{
+    for (UITouch* touch in touches) {
+        if (touch != _touch) {
+            continue;
+        }
+        
+        _touch = nil;
+        
+        if (_playingSound) {
+            
+            [[SoundManager defaultManager] stopNoise];
+            
+            _playingSound = NO;
+        }
+    }
+}
+
+// .............................................................................
+
+- (void) touchesCancelled:(NSSet*) touches
+                withEvent:(UIEvent*) event
+{
+    [self touchesEnded:touches
+             withEvent:event];
+}
+
+// .............................................................................
+
+-(void) update:(CFTimeInterval) currentTime
+{
+    _currentTime = currentTime;
+    
+    if (_lastTime) {
+        
+        _dt = currentTime - _lastTime;
+    }
+    else{
+        // First frame - fake deltaTime:
+        
+        _dt = 1.0f / 60.0f;
+    }
+    
+    _lastTime = currentTime;
+
+    
+    if (_playingSound && _dragging) {
+        
+        SoundManager* soundManager = [SoundManager defaultManager];
+        
+        CGFloat gain = _dragCurrentSpeed/20000.0;
+        
+        if (gain > 0.25f) {
+            gain = 0.25f;
+        }
+        
+        [soundManager setNoiseGain:gain];
+        [soundManager setBandpassFilterBandwidth:400.0f];
+        [soundManager setBandpassFilterCenterFrequency:2700.0f + gain*20500.0f];
+        
+        
+        // Fade out speed
+        _dragCurrentSpeed *= 0.5f;
+    }
 }
 
 @end
--- Source/iOS Pencil Sound Demo/Project Files/Classes/ViewController.m
+++ Source/iOS Pencil Sound Demo/Project Files/Classes/ViewController.m
@@ -9,43 +9,65 @@
 #import "ViewController.h"
 #import "TestScene.h"
 
-@implementation ViewController
 
-- (void)viewDidLoad
+// .............................................................................
+
+@implementation ViewController
+{
+    
+}
+
+
+// .............................................................................
+
+- (void) viewDidLoad
 {
     [super viewDidLoad];
 
     // Configure the view.
-    SKView * skView = (SKView *)self.view;
-    skView.showsFPS = YES;
-    skView.showsNodeCount = YES;
+    SKView* skView = (SKView*) [self view];
+    
+    //[skView setShowsFPS:YES];
+    //[skView setShowsNodeCount:YES];
     
     // Create and configure the scene.
-    SKScene * scene = [TestScene sceneWithSize:skView.bounds.size];
-    scene.scaleMode = SKSceneScaleModeAspectFill;
+    SKScene* scene = [TestScene sceneWithSize:skView.bounds.size];
+
+    [scene setScaleMode:SKSceneScaleModeAspectFill];
+    
     
     // Present the scene.
     [skView presentScene:scene];
 }
 
-- (BOOL)shouldAutorotate
+// .............................................................................
+
+- (BOOL) shouldAutorotate
 {
     return YES;
 }
 
-- (NSUInteger)supportedInterfaceOrientations
+// .............................................................................
+
+- (NSUInteger) supportedInterfaceOrientations
 {
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+    if([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone){
+        
         return UIInterfaceOrientationMaskAllButUpsideDown;
-    } else {
+    }
+    else{
         return UIInterfaceOrientationMaskAll;
     }
 }
 
-- (void)didReceiveMemoryWarning
+// .............................................................................
+
+- (void) didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Release any cached data, images, etc that aren't in use.
 }
+
+// .............................................................................
 
 @end
